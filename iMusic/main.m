6113acb1d38137bcb681b2dce4bedb85924ca4e0
uzaysu <uzaysu@gmail.com>
2013-11-18 12:30:06 +0200

    Initial Commit

---
+++ iMusic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iMusic
+//
+//  Created by selcuk.cigir on 18/11/13.
+//  Copyright (c) 2013 selcuk.cigir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iMusicTests/iMusicTests.m
@@ -0,0 +1,34 @@
+//
+//  iMusicTests.m
+//  iMusicTests
+//
+//  Created by selcuk.cigir on 18/11/13.
+//  Copyright (c) 2013 selcuk.cigir. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iMusicTests : XCTestCase
+
+@end
+
+@implementation iMusicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
